03f20fa4f5c760e797b7c1d37beb049c3b3e79c7
Erdem Serdaroğlu <erdemser@hotmail.com>
2024-04-19 00:27:44 +0300

    Profile update

--- person-web/src/Profilecontent.jsx
+++ person-web/src/Profilecontent.jsx
@@ -3,8 +3,50 @@ import React, { Component } from 'react'
 export default class Profilecontent extends Component {
   render() {
     return (
-      <div>
-        <div className='ProfileText'>Profile</div>
+      <div className='container-fluid ProfileContainer'>
+        <div className='row align-items-center justify-content-center h-100 flex-column'>
+          <div className='row justify-content-center'>
+            <div className='col-9'>
+              <span className='ProfileSpan'>Profile</span>
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='col-3'>
+              <div className='col-12 ProfileHeader mb-2'>Basic Information</div>
+              <div className='row ProfileContent mb-2'>
+                <div className='col'>Doğum Tarihi</div>
+                <div className='col'>24.03.2024</div>
+              </div>
+              <div className='row ProfileContent mb-2'>
+                <div className='col'>İkamet Şehri</div>
+                <div className='col'>Ankara</div>
+              </div>
+              <div className='row ProfileContent mb-2'>
+                <div className='col'>Eğitim Durumu</div>
+                <div className='col'>Hacettepe Ünv.  Biyoloji Lisans, 2016</div>
+              </div>
+              <div className='row ProfileContent mb-2'>
+                <div className='col'>Tercih Ettiği Rol</div>
+                <div className='col'>Frontend, UI</div>
+              </div>
+            </div>
+            <div className='col-3' style={{justifyContent: "center", display: "flex"}}>
+              <div className='ImageDiv2'></div>
+            </div>
+            <div className='col-3'>
+              <div className='col-12 ProfileHeader'>About Me</div>
+              <div className='col-12 ProfileContent'>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.
+                </p>
+                <p>
+                  Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        {/* <div className='ProfileText'>Profile</div>
         <div className='ProfileContent'>
             <div className='Basicİnfo'>
                 <h1>Basic Information</h1>
@@ -17,8 +59,8 @@ export default class Profilecontent extends Component {
                 <h1>About Me</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta perferendis corporis optio sequi, ex illum non repudiandae ipsa dicta unde, sint ea consequatur necessitatibus placeat asperiores laborum, sapiente porro aperiam!</p>
             </div>
-        </div>
-     </div> 
+        </div> */}
+      </div>
     )
   }
 }
