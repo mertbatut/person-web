6935e926c30cef710dde94d7c6858044ed2a02a2
mertbatut <mertbatut@gmail.com>
2024-04-19 00:30:15 +0300

    Projects

--- person-web/src/Profilecontent.jsx
+++ person-web/src/Profilecontent.jsx
@@ -4,21 +4,32 @@ export default class Profilecontent extends Component {
   render() {
     return (
       <div>
-        <div className='ProfileText'>Profile</div>
+
         <div className='ProfileContent'>
-            <div className='Basicİnfo'>
-                <h1>Basic Information</h1>
-                <div className='Profileİnfo'>
-                    <p>Doğum Tarihi</p>
-                </div>
+          <div className='ProfileText'>Profile</div>
+          <div className='Basicİnfo'>
+            <h1>Basic Information</h1>
+            <div className='Profileİnfo'>
+              <p>Doğum Tarihi: </p>
+              <p>İkamet Şehri: </p>
+              <p>Eğitim Durumu: </p>
+              <p>Tercih Ettiği Rol: </p>
             </div>
-            <img src="#" alt="" />
-            <div className='AboutBox'>
-                <h1>About Me</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta perferendis corporis optio sequi, ex illum non repudiandae ipsa dicta unde, sint ea consequatur necessitatibus placeat asperiores laborum, sapiente porro aperiam!</p>
+            <div className='ProfileInfo2'>
+              <p>24.03.1996</p>
+              <p>Ankara</p>
+              <p>Hacettepe Ünv.  Biyoloji
+                Lisans, 2016</p>
+              <p>Frontend, UI</p>
             </div>
+          </div>
+          <img style={{width:"300px" , height:"290px", borderRadius:"10px"}} src="profile2.png" alt="" />
+          <div className='AboutBox'>
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta perferendis corporis optio sequi, ex illum non repudiandae ipsa dicta unde, sint ea consequatur necessitatibus placeat asperiores laborum, sapiente porro aperiam!</p>
+          </div>
         </div>
-     </div> 
+      </div>
     )
   }
 }
