593fc02d9a10f98b159ad3fc505c8bc5542eb83b
mertbatut <mertbatut@gmail.com>
2024-04-18 20:53:57 +0300

    Footer

--- person-web/src/App.jsx
+++ person-web/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import HeroSection from './Hero.jsx'
 import Skills from './Skills.jsx'
 import Profilecontent from './Profilecontent.jsx'
-
+import Footer from './Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +14,7 @@ function App() {
         <HeroSection/>
         <Skills/>
         <Profilecontent/>
-
+        <Footer></Footer>
       </div>
       
     </>
