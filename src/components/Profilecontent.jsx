8fab5b537569464e33ecd33f1c69358102e2027f
mertbatut <mertbatut@gmail.com>
2024-06-06 16:40:36 +0300

    Profile Tailwind

--- src/components/Profilecontent.jsx
+++ src/components/Profilecontent.jsx
@@ -3,43 +3,45 @@ import React, { Component } from 'react'
 export default class Profilecontent extends Component {
   render() {
     return (
-      <div className='container-fluid ProfileContainer'>
-        <div className='row align-items-center justify-content-center h-100 flex-column'>
-          <div className='row justify-content-center'>
-            <div className='col-9'>
-              <span className='ProfileSpan'>Profile</span>
+      <div className='bg-[#4731D3]'>
+        <div className='flex gap-24 justify-center items-center'>
+        <span className='text-[48px] font-bold text-[#CBF281]'>Profil</span>
+          <div className=' '>
+            
+            <div className=''>
+              
             </div>
           </div>
-          <div className='row justify-content-center'>
-            <div className='col-3'>
-              <div className='col-12 ProfileHeader mb-2'>Basic Information</div>
-              <div className='row ProfileContent mb-2'>
-                <div className='col'>Doğum Tarihi</div>
-                <div className='col'>24.03.2024</div>
+          <div className='flex gap-12 items-center'>
+            <div className='[300px] h-[290px] flex gap-[24px] flex-col'>
+              <div className='font-semibold text-[30px] text-[#FFFFFF] '>Temel Bilgiler</div>
+              <div className='flex gap-8'>
+                <div className='font-semibold text-[16px] text-[#CBF281]'>Doğum Tarihi :</div>
+                <div className='font-normal text-[16px] text-[#FFFFFF]'>24.03.2024</div>
               </div>
-              <div className='row ProfileContent mb-2'>
-                <div className='col'>İkamet Şehri</div>
-                <div className='col'>Ankara</div>
+              <div className='flex gap-8'>
+                <div className='font-semibold text-[16px] text-[#CBF281]'>İkamet Şehri :</div>
+                <div className='font-normal text-[16px] text-[#FFFFFF] pl-2'>Ankara</div>
               </div>
-              <div className='row ProfileContent mb-2'>
-                <div className='col'>Eğitim Durumu</div>
-                <div className='col'>Hacettepe Ünv.  Biyoloji Lisans, 2016</div>
+              <div className='flex gap-8'>
+                <div className='font-semibold text-[16px] text-[#CBF281]'>Eğitim Durumu</div>
+                <div className='font-normal text-[16px] text-[#FFFFFF]'>Hacettepe Ünv.  Biyoloji Lisans, 2016</div>
               </div>
-              <div className='row ProfileContent mb-2'>
-                <div className='col'>Tercih Ettiği Rol</div>
-                <div className='col'>Frontend, UI</div>
+              <div className='flex gap-8'>
+                <div className='font-semibold text-[16px] text-[#CBF281]'>Tercih Ettiği Rol</div>
+                <div className='font-normal text-[16px] text-[#FFFFFF]'>Frontend, UI</div>
               </div>
             </div>
-            <div className='col-3' style={{justifyContent: "center", display: "flex"}}>
-              <div className='ImageDiv2'></div>
+            <div className='ProImage py-12 '>
+              <div className=' '><img className='rounded-xl w-[300px] h-[290px]' src="https://s3-alpha-sig.figma.com/img/ec36/93fd/2fc15cffc612b2448fb1f38e4c8c72fd?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p70T21cjHi~Vv1ZsQaryHJd1YcapmluNAG7juNSFVHCfXs67gH2w~0QABWKOzF2qmIwgs5tY94LJChKK~JQU1eYk-V7uPPCouYa8B6gonFrGddt0Rz-9geeOqP3uRy1Go0WRDWDwzLe9pvudApEH-~orWS4a8frVlTge0RAgHHsogDa-VdajGgjzpTkSnJJc5q4VKi9XVGG9S3tWx3XebneHPD6AYSrxTVy2plTO4hio~-QL1OlsXgocRPw9XSj~~5uDSgNTuo78zxr7uZS1LuA-dp2JiydwhpBcu96O7vGZ7gVGOlZ~Bl1sbQXUTRpNJ18r0NXzlaGPF6rqDgd-Ow__" alt="" /></div>
             </div>
-            <div className='col-3 flex flex-col gap-12'>
-              <div className='col-12 ProfileHeader'>HAKKIMDA</div>
-              <div className='col-12 ProfileContent'>
-                <p>
+            <div className='w-[300px] h-[290px] flex flex-col gap-8'>
+              <div className='font-normal text-[30px] text-[#FFFFFF]'>HAKKIMDA</div>
+              <div className=' '>
+                <p className='font-normal text-[16px] text-[#FFFFFF]'>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.
                 </p>
-                <p>
+                <p className='font-normal text-[16px] text-[#FFFFFF]'>
                   Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!</p>
               </div>
             </div>
