5009af961dfab40ab07e3293d0cc1d006819ad73
mertbatut <mertbatut@gmail.com>
2024-06-06 17:50:35 +0300

    Footer Tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ export default class Footer extends Component {
     render() {
         return (
             <div>
-                <div className='Footer1' style={{ height: "454px", background: "#F9F9F9" }}>
-                    <p style={{ fontSize: "48px", fontWeight: "700" }}>Send me a message!</p>
-                    <p id='FooterP' style={{ fontSize: "24px", fontWeight: "400", color: "#120B39", width: "448px" }}>Got a question or proposal, or just want to say hello? Go ahead.</p>
-                    <p style={{ fontSize: "20px", textDecoration: "underline" }}>almilasucode@gmail.com</p>
-                    <div className='FooterIcon'>
-                        <img src="https://placehold.co/35x35" alt="" style={{ marginRight: "1rem" }} />
-                        <img src="https://placehold.co/35x35" alt="" />
-                        <img src="https://placehold.co/35x35" alt="" style={{ marginLeft: "1rem" }} />
-                        <img src="https://placehold.co/35x35" alt="" style={{ marginLeft: "1rem" }} />
+                <div className='Footer1 flex flex-col items-center py-16 bg-[#F9F9F9] gap-8' >
+                    <p className='text-5xl font-bold text-[#4731D3]'>Send me a message!</p>
+                    <p className='text-2xl font-normal text-[#120B39]' >Got a question or proposal, or just want to say hello? Go ahead.</p>
+                   <a href=""> <p className='text-xl font-medium text-[#4731D3]'>mertbatut@gmail.com</p> </a>
+                    <div className='FooterIcon flex gap-4'>
+                        <img src="https://placehold.co/35x35" alt=""/>
+                        <img src="https://placehold.co/35x35" alt=""/>
+                        <img src="https://placehold.co/35x35" alt=""/>
+                        <img src="https://placehold.co/35x35" alt=""/>
                     </div>
 
                 </div>
